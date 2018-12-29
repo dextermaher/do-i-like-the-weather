a69f76156518c3a27a04efbf6ed29ead25edef5f
Dexter Maher <dextermaher@Matts-MacBook.local>
2018-12-28 19:05:22 -0800

    Dynamic photos!

--- src/TestUI.js
+++ src/TestUI.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 class TestUI extends Component {
     constructor(props) {
         super(props);
-        this.fetchCurrentConditions();
+
         this.state = {
-          currentTemp : '--',
-          currentConditions : '--',
-          conditionsImageURL : 'https://pbs.twimg.com/profile_images/966804932866162688/RsZEA9iJ.jpg',
-          weatherExampleURL : 'https://cdn-images-1.medium.com/max/1200/1*_EP0OeIPcQf-XiDyRSkoAQ.jpeg',
-    
+            currentTemp : '--',
+            currentConditions : '--',
+            conditionsImageURL : 'https://pbs.twimg.com/profile_images/966804932866162688/RsZEA9iJ.jpg',
+            weatherExampleURL : 'https://cdn-images-1.medium.com/max/1200/1*_EP0OeIPcQf-XiDyRSkoAQ.jpeg',    
         }
+
+        this.updateWeatherInfo();
       }
       setTemp = (ev) => {
         ev.preventDefault(); 
@@ -26,10 +27,18 @@ class TestUI extends Component {
           }
         );
       }
-    
+      updateWeatherInfo() {
+        this.fetchCurrentConditions()
+            .then(() => {
+                this.fetchPhotoForPhrase();
+            }) 
+            .catch((er) =>{
+                console.log(er);
+            });
+      }
       fetchCurrentConditions(){
-        const url = 'http://dataservice.accuweather.com/currentconditions/v1/39671_PC?apikey=rvSVrxAEhkTPZ8Zzou6hLusbiaZAobB9';
-        fetch(url).then((fresp) => fresp.json())
+        const url = 'http://dataservice.accuweather.com/currentconditions/v1/39671_PC?ACCUWE&apikey=rvSVrxAEhkTPZ8Zzou6hLusbiaZAobB9';
+        return fetch(url).then((fresp) => fresp.json())
                    .then( (cwr) => 
                     { 
                       console.log(cwr);
@@ -38,10 +47,33 @@ class TestUI extends Component {
                             {
                               currentTemp : cwr[0].Temperature.Imperial.Value,
                               currentConditions : cwr[0].WeatherText,
-        
                             }
                           );
                       }
+                    })
+                    .catch((er) =>{
+                        console.log(er);
+                    })
+      }
+
+      fetchPhotoForPhrase() {
+        const {currentConditions} = this.state;
+        // encodeURI('http://www.here.com/this that')
+
+        let phrase = `${currentConditions || 'foggy'} skies`;
+        // phrase = 'rain';
+        // phrase = 'cloudy';
+        // phrase = 'snow';
+        // phrase = 'hail';
+        
+// todo: use flickr group to search
+        const photoDataUrl = `https://api.flickr.com/services/rest/?FLICKR&text=${phrase}&method=flickr.photos.search&api_key=c69b8f9f5fee24232d061c0133679430&format=json&nojsoncallback=1`;
+        fetch(photoDataUrl).then((fresp) => fresp.json())
+                   .then( (flickrSearchResponse) => 
+                    { 
+                        const photoData = flickrSearchResponse.photos.photo[0];
+                        const photoUrl = `https://farm${photoData.farm}.staticflickr.com/${photoData.server}/${photoData.id}_${photoData.secret}.jpg`;
+                        this.setState({ weatherExampleURL : photoUrl })
                     })
                     .catch((er) =>{
                         console.log(er);
@@ -52,8 +84,10 @@ class TestUI extends Component {
         const { currentTemp, 
                 currentConditions, 
                 conditionsImageURL, 
-                weatherExampleURL} = this.state;
+                weatherExampleURL,
+            } = this.state;
     
+        
         return (
           <div>
             <div className='toolbar'>
